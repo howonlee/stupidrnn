4bb3f9d7ec0f6ba4c1584c65c638408c00c1a476
Howon Lee <howon@howonlee.com>
2016-02-15 15:31:27 -0800

    input fails

--- srn.py
+++ srn.py
@@ -5,7 +5,6 @@
 
 import tensorflow as tf
 import numpy as np
-import input_data
 import time
 import operator as op
 
@@ -14,29 +13,34 @@ def onehot(idx, char_len):
     arr[idx] = 1.0
     return arr
 
-num_nets = 25
-num_hiddens = 200
-num_epochs = 20
+num_nets = 5
+num_hiddens = 400
+num_epochs = 50
 
 with open("corpus.txt") as corpus_file:
-    chars = list(corpus_file.read())[:300000]
+    chars = list(corpus_file.read())[:100000]
     char_len = len(set(chars))
     char_to_idx = {char:onehot(idx, char_len) for idx, char in enumerate(list(set(chars)))}
-    all_data = []
-    for fst, snd in zip(chars, chars[1:]):
-        all_data.append((char_to_idx[fst], char_to_idx[snd]))
-    train_len = (19 * len(all_data)) // 20
-    train_list = all_data[:train_len]
-    test_list = all_data[train_len:]
-    trX = np.vstack(tuple(map(op.itemgetter(0), train_list)))
-    teX = np.vstack(tuple(map(op.itemgetter(0), test_list)))
-    trY = np.vstack(tuple(map(op.itemgetter(1), train_list)))
-    teY = np.vstack(tuple(map(op.itemgetter(1), test_list)))
+    trXs, teXs, trYs, teYs = [], [], [], []
+    for net_idx in xrange(num_nets):
+        print net_idx
+        all_data = []
+        # for fst, snd in zip(chars, chars[net_idx+1:]):
+        for fst, snd in zip(chars, chars[1:]):
+            all_data.append((char_to_idx[fst], char_to_idx[snd]))
+        train_len = (19 * len(all_data)) // 20
+        train_list = all_data[:train_len]
+        test_list = all_data[train_len:]
+        trXs.append(np.vstack(tuple(map(op.itemgetter(0), train_list))))
+        teXs.append(np.vstack(tuple(map(op.itemgetter(0), test_list))))
+        trYs.append(np.vstack(tuple(map(op.itemgetter(1), train_list))))
+        teYs.append(np.vstack(tuple(map(op.itemgetter(1), test_list))))
+    # trXs.reverse()
+    # teXs.reverse()
+    # trYs.reverse()
+    # teYs.reverse()
 print "finished processing corpus"
 
-
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-# trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
 
 input_dim = char_len
 output_dim = char_len
@@ -69,24 +73,26 @@ sess = tf.Session()
 init = tf.initialize_all_variables()
 sess.run(init)
 
-curr_trX = trX[:]
-curr_teX = teX[:]
+curr_trX = trXs[0][:]
+curr_teX = teXs[0][:]
 for net_idx, curr_train_ops in enumerate(train_ops):
     print "=================="
     print "net : ", net_idx
     print "=================="
-    te_fd = {Y: teY}
+    te_fd = {Y: teYs[net_idx]}
     te_fd[locals()["X" + str(net_idx)]] = curr_teX
     for i in range(num_epochs):
-        for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-            tr_fd = {Y: trY[start:end]}
+        for start, end in zip(range(0, len(trXs[net_idx]), 128), range(128, len(trXs[net_idx]), 128)):
+            tr_fd = {Y: trYs[net_idx][start:end]}
             tr_fd[locals()["X" + str(net_idx)]] = curr_trX[start:end]
             sess.run(curr_train_ops, feed_dict=tr_fd)
-        curr_acc = np.mean(np.argmax(teY, axis=1) ==
+        curr_acc = np.mean(np.argmax(teYs[net_idx], axis=1) ==
                            sess.run(predict_ops[net_idx], feed_dict=te_fd))
         print i, curr_acc, time.clock()
         # use prediction accuracy because I can't be bothered
-    total_tr_fd = {Y: trY[:]}
+    total_tr_fd = {Y: trYs[net_idx]}
     total_tr_fd[locals()["X" + str(net_idx)]] = curr_trX[:]
-    curr_trX = np.hstack((trX, hs[net_idx].eval(session=sess, feed_dict=total_tr_fd)))
-    curr_teX = np.hstack((teX, hs[net_idx].eval(session=sess, feed_dict=te_fd)))
+    # fix this properly
+    if net_idx < (num_nets-1):
+        curr_trX = np.hstack((trXs[net_idx][net_idx:], hs[net_idx].eval(session=sess, feed_dict=total_tr_fd)))
+        curr_teX = np.hstack((teXs[net_idx], hs[net_idx].eval(session=sess, feed_dict=te_fd)))
