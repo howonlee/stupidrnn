72ba7a2e1c0153810bc1a59c27282a5ac3075231
Howon Lee <howon@howonlee.com>
2016-02-15 11:38:45 -0800

    they are getting changed, anyhow

--- srn.py
+++ srn.py
@@ -31,9 +31,10 @@ sess = tf.Session()
 init = tf.initialize_all_variables()
 sess.run(init)
 
+print list(h.eval(session=sess, feed_dict={X: teX, Y: teY})[0, :])
 for i in range(12):
     for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
         sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
     print i, np.mean(np.argmax(teY, axis=1) ==
                      sess.run(predict_op, feed_dict={X: teX, Y: teY}))
-    print h.eval(session=sess, feed_dict={X: teX, Y: teY}).shape
+    print list(h.eval(session=sess, feed_dict={X: teX, Y: teY})[0, :])
