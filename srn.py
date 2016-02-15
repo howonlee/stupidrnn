b7b25d7b4cf6d4f9725646b8b7138895ae7871db
Howon Lee <howon@howonlee.com>
2016-02-15 11:34:37 -0800

    hiddens properly seen

--- srn.py
+++ srn.py
@@ -17,8 +17,8 @@ trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, 
 X = tf.placeholder("float", [None, 784])
 Y = tf.placeholder("float", [None, 10])
 
-w_h = init_weights([784, 625]) # create symbolic variables
-w_o = init_weights([625, 10])
+w_h = tf.Variable(tf.random_normal([784, 625], stddev=0.01))
+w_o = tf.Variable(tf.random_normal([625, 10], stddev=0.01))
 
 h = tf.nn.sigmoid(tf.matmul(X, w_h)) # this is a basic mlp, think 2 stacked logistic regressions
 py_x = tf.matmul(h, w_o)
@@ -36,4 +36,4 @@ for i in range(12):
         sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
     print i, np.mean(np.argmax(teY, axis=1) ==
                      sess.run(predict_op, feed_dict={X: teX, Y: teY}))
-    print h.eval(session=sess, feed_dict={X: teX, Y: teY})
+    print h.eval(session=sess, feed_dict={X: teX, Y: teY}).shape
