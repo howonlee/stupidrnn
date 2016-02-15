ccef9ef8da6d63b72aa140002cceb976ccc6b12c
Howon Lee <howon@howonlee.com>
2016-02-15 12:55:46 -0800

    some commentary

--- srn.py
+++ srn.py
@@ -6,6 +6,7 @@
 import tensorflow as tf
 import numpy as np
 import input_data
+import time
 
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -28,11 +29,10 @@ w_os = [tf.Variable(tf.random_normal([625, 10], stddev=0.01))]
 for x in xrange(num_nets):
     w_os.append(tf.Variable(tf.random_normal([625, 10], stddev=0.01)))
 
-# this is a basic mlp, think 2 stacked logistic regressions
 hs = [tf.nn.sigmoid(tf.matmul(Xs[idx], w_h)) for idx, w_h in enumerate(w_hs)]
 py_xs = [tf.matmul(h, w_os[x]) for x, h in enumerate(hs)]
 
-costs = [tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y)) for py_x in py_xs] # compute costs
+costs = [tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y)) for py_x in py_xs]
 train_ops = [tf.train.GradientDescentOptimizer(0.05).minimize(cost) for cost in costs]
 predict_ops = [tf.argmax(py_x, 1) for py_x in py_xs]
 
@@ -43,6 +43,9 @@ sess.run(init)
 curr_trX = trX[:]
 curr_teX = teX[:]
 for net_idx, curr_train_ops in enumerate(train_ops):
+    print "=================="
+    print "net : ", net_idx
+    print "=================="
     te_fd = {Y: teY}
     te_fd[locals()["X" + str(net_idx)]] = curr_teX
     for i in range(10):
@@ -50,8 +53,9 @@ for net_idx, curr_train_ops in enumerate(train_ops):
             tr_fd = {Y: trY[start:end]}
             tr_fd[locals()["X" + str(net_idx)]] = curr_trX[start:end]
             sess.run(curr_train_ops, feed_dict=tr_fd)
-        print i, np.mean(np.argmax(teY, axis=1) ==
-                         sess.run(predict_ops[net_idx], feed_dict=te_fd))
+        curr_acc = np.mean(np.argmax(teY, axis=1) ==
+                           sess.run(predict_ops[net_idx], feed_dict=te_fd))
+        print i, curr_acc, time.clock()
     total_tr_fd = {Y: trY[:]}
     total_tr_fd[locals()["X" + str(net_idx)]] = curr_trX[:]
     curr_trX = hs[net_idx].eval(session=sess, feed_dict=total_tr_fd)
