9953934d42df6d14cd9de416523f5e7bc81a5540
Howon Lee <howon@howonlee.com>
2016-02-15 16:01:38 -0800

    killed shape problem, some progress

--- srn.py
+++ srn.py
@@ -15,7 +15,7 @@ def onehot(idx, char_len):
 
 num_nets = 10
 num_hiddens = 400
-num_epochs = 1
+num_epochs = 30
 
 with open("corpus.txt") as corpus_file:
     chars = list(corpus_file.read())[:100000]
@@ -25,18 +25,20 @@ with open("corpus.txt") as corpus_file:
     for net_idx in xrange(num_nets):
         print net_idx
         all_data = []
-        for fst, snd in zip(chars, chars[1:]):
-        # for fst, snd in zip(chars, chars[net_idx+1:]):
+        # for fst, snd in zip(chars, chars[1:]):
+        for fst, snd in zip(chars, chars[net_idx+1:]):
             all_data.append((char_to_idx[fst], char_to_idx[snd]))
         train_len = (19 * len(all_data)) // 20
-        train_list = all_data[:train_len]
-        test_list = all_data[train_len:]
+        train_list = all_data[:train_len][net_idx:]
+        test_list = all_data[train_len:][net_idx:]
         trXs.append(np.vstack(tuple(map(op.itemgetter(0), train_list))))
         teXs.append(np.vstack(tuple(map(op.itemgetter(0), test_list))))
         trYs.append(np.vstack(tuple(map(op.itemgetter(1), train_list))))
         teYs.append(np.vstack(tuple(map(op.itemgetter(1), test_list))))
     # for member in trXs:
-    #     print member.shape
+    #     print list(member[:5])
+    # for member in teXs:
+    #     print list(member[:5])
     # for member in teXs:
     #     print member.shape
 print "finished processing corpus"
@@ -94,5 +96,5 @@ for net_idx, curr_train_ops in enumerate(train_ops):
     total_tr_fd[locals()["X" + str(net_idx)]] = curr_trX[:]
     # fix this properly
     if net_idx < (num_nets-1):
-        curr_trX = np.hstack((trXs[net_idx+1], hs[net_idx].eval(session=sess, feed_dict=total_tr_fd)))
-        curr_teX = np.hstack((teXs[net_idx+1], hs[net_idx].eval(session=sess, feed_dict=te_fd)))
+        curr_trX = np.hstack((trXs[net_idx+1], hs[net_idx].eval(session=sess, feed_dict=total_tr_fd)[2:]))
+        curr_teX = np.hstack((teXs[net_idx+1], hs[net_idx].eval(session=sess, feed_dict=te_fd)[1:]))
